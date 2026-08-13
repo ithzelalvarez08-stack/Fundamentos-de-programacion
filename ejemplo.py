2d56e198679811ba141b59682cac7752045e395a
Ithzel Alvarez <ithzel.alvarez.08@gamil.com>
2026-08-13 12:00:44 -0600

    Su primer commit

--- ejemplo.py
+++
@@ -1,9 +0,0 @@
-
-print("Bienvenido, este programa te ayudara a encontrar tu IMC")
-nombre=(input("Ingresa tu nombre: "))
-p=float(input("¿Cual es tu peso? "))
-a=float(input("¿Cual es tu altura? "))
-
-imc= str(p/(a**2))
-
-print("Tu indice de masa corporal ", nombre,"es: ", imc)
