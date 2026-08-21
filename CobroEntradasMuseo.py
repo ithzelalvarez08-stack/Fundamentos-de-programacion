7ec39f70101496bf4ae068278bc761ffd68cea71
Ithzel Alvarez <ithzel.alvarez.08@gamil.com>
2026-08-20 21:27:14 -0600

    hasta este punto se guardaron las correciones del programa actividad 2 y tambien algunos ejercicios extras evaluables

--- CobroEntradasMuseo.py
+++ CobroEntradasMuseo.py
@@ -7,8 +7,10 @@ descuento_adulto = 0.12
 descuento_profesor = 0.10
 descuento_estudiante = 0.10
 descuento_ninguno = 0.0
+
 total=0.0
 descuento=0.0
+
 print("===============================================================================")
 print("=================== SISTEMA DE COBRO, MUSEO DE ANTROPOLIGIA ===================")
 print("===============================================================================")
@@ -24,8 +26,8 @@ print("                                                ")
 print("                                                ")
 t_visitantes= int(input("INGRESE LA CANTIDAD DE VISITANTES: "))
 
-for i in range(1,t_visitantes +1):
-    print(f"\n ============== Visitante {i}  ===============")
+for i in range(t_visitantes):
+    print(f"\n ============== Visitante {i + 1}  ===============")
 
     edad=int(input("Ingrese la edad del visitante: "))
     #ELECCION DEL PRECIO SEGUN LA EDAD
@@ -37,17 +39,20 @@ for i in range(1,t_visitantes +1):
         precio_inicial = precio_mayor_edad
 
     #ELECCION SOBRE EL TIPO DE VISITANTE
-
-    visitante= input("SELECCIONE EL TIPO DE VISITANTE (A)ADULTO MAYOR, (B)PROFESOR, (C)ESTUDIANTE ")
-    # DESCUENTOS POR PERSONA (APLICA SOLO UNO POR PERSONA)
-    if (visitante in ["adulto mayor", "a"]):
-        descuento = descuento_adulto
-    elif (visitante in ( "profesor", "b")):
-        descuento = descuento_profesor
-    elif (visitante in ( "estudiante", "c")):
-        descuento = descuento_estudiante
+    if edad<3:
+         descuento == descuento_ninguno
     else:
-        descuento == descuento_ninguno
+        visitante= input("SELECCIONE EL TIPO DE VISITANTE (A)ADULTO MAYOR, (B)PROFESOR, (C)ESTUDIANTE ")
+    # DESCUENTOS POR PERSONA (APLICA SOLO UNO POR PERSONA)
+        if (visitante in ["adulto mayor", "a"]):
+            descuento = descuento_adulto
+        elif (visitante in ( "profesor", "b")):
+            descuento = descuento_profesor
+        elif (visitante in ( "estudiante", "c")):
+            descuento = descuento_estudiante
+        else:
+            print("MENORES DE 3 AÑOS NO PAGAN")
+  
 
     descuento_final = (precio_inicial * descuento)
     sub_total = (precio_inicial - descuento_final)
